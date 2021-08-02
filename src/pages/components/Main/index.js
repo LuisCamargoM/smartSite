e5d59e2f8e7add815787a83558c02e7390d000d4
Luis Camargo <lc@bullrobotinternational.com>
2021-08-01 22:40:58 -0300

    Beta v1.1 - Fix non call Components Done

--- src/pages/components/Main/index.js
+++ src/pages/components/Main/index.js
@@ -1,8 +1,6 @@
 import './Main.css';
 import SubHeader from './components/SubHeader';
 import Form from './components/Form';
-import Label from './components/Label';
-import Result from './components/Result';
 
 const Main = () => {    
   return (
